8260ced6074812135c3e40c50a78bfee2b44e5b7
ksharugeshwaran <sharugeshwaran.k@kalvium.community>
2024-01-30 10:06:56 +0530

    Added error handling

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ const express = require("express")
 const app = express()
 
 app.get("/ping",(req,res) => {
-    res.send("Hi This is Waran")
+    res.send("Hi")
 })
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send("Something went wrong!");
+});
 
 app.listen(3000,() => {
     console.log("Running App on port 3000")
