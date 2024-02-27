177c042689981dbb464deada8e723bb159562ae6
ksharugeshwaran <sharugeshwaran.k@kalvium.community>
2024-02-27 14:22:24 +0530

    bug fixing

--- Backend/routes/routers.js
+++ Backend/routes/routers.js
@@ -30,6 +30,7 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
+    console.log(req.body)
     try {
         console.log(req.body)
         const { error } = postSchema.validate(req.body);
